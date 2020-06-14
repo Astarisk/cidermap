9a2a6232d0dd5a50d7947938d186556afcdd009e
Astarisk <astarisk@gmail.com>
2020-06-14 12:39:12 -0400

    Restructured file structure.

--- cidermap_project/cidermap/myapi/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Grid
-
-
-class GridSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Grid
-        fields = ['grid_id', 'x_coord', 'y_coord']
