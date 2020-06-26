7653b525676c0bdefb7c0fae531ce2e6c1efd68a
Astarisk <astarisk@gmail.com>
2020-06-25 21:29:52 -0400

    Split server specific settings into its own file.

--- config/settings.py
+++ config/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my-app-db',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'db',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
