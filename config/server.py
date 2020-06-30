b4cf137cb98d9ad278483eea2b94cb7578f5a19e
Astarisk <astarisk@gmail.com>
2020-06-30 00:55:16 -0400

    Swapping to postgres

--- config/server.py
+++ config/server.py
@@ -9,11 +9,11 @@ SECRET_KEY = 'nn&5!(a6+cl76#smtmu3n27s-7!rgg3=h&6y0hhxg5zl&o17as'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my-app-db',
-        'USER': 'root',
-        'PASSWORD': 'password',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
-        'PORT': 3306,
+        'PORT': 5432,
     }
 }
